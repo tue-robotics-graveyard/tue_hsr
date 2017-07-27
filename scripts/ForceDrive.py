ac07ca2e58df21ee7d1c61870196fea3ca5e878b
Rein Appeldoorn <reinzor@gmail.com>
2017-07-27 13:59:52 +0200

    wip

--- scripts/ForceDrive.py
+++ scripts/ForceDrive.py
@@ -12,33 +12,19 @@ class ForceDrive:
     def drive(self, vx, vy, vth, timeout):
 
         v = geometry_msgs.msg.Twist()        # Initialize velocity
-
-        # When using simulated Clock time, get_rostime() returns time 0 until first message has been received on /clock,
-        # so 0 means essentially that the client does not know clock time yet.
-        # A value of 0 should therefore be treated differently,
-        # such as looping over get_rostime() until non-zero is returned.
-
-        # Wait for time to run :-)
-
-        t_check = rospy.Time.now()
-
-        while (rospy.Time.now() - t_check) < rospy.Duration.from_sec(1):
-            rospy.sleep(0.1)
-
-        # now really start
-        t_start = rospy.Time.now()
-
-        print rospy.Time.now()
-        # Drive
         v.linear.x = vx
         v.linear.y = vy
         v.angular.z= vth
 
+        # now really start
+        t_start = rospy.Time.now()
+
+        # Drive
         rospy.loginfo("Starting to drive")
 
         while (rospy.Time.now() - t_start) < rospy.Duration.from_sec(timeout):
             self._cmd_vel.publish(v)
-            rospy.sleep(0.1)
+            rospy.sleep(0.01)
 
         rospy.loginfo("Finished driving")
         # Stop driving
